dc0c8012d0b81865ccab72b1789230dcb21695a5
da7th-laptop <m.da7th@gmail.com>
2016-09-23 20:48:17 +0300

    completed main

--- app/src/main/java/com/example/android/popularmoviesapp/MovieAdapater.java
+++ app/src/main/java/com/example/android/popularmoviesapp/MovieAdapater.java
@@ -6,6 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -34,6 +37,9 @@ public class MovieAdapater extends ArrayAdapter<Movie> {
 
 
         //set the data to their views
+
+        ImageView gridImage = (ImageView) gridView.findViewById(R.id.grid_item);
+        Picasso.with(getContext()).load(currentMovie.getPosterPath()).into(gridImage);
 
 
         return gridView;
