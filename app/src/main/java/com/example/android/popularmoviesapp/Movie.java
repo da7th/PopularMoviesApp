7902b9197ffe91de181ca3d78634b88ace7b8be8
da7th-laptop <m.da7th@gmail.com>
2016-09-23 14:58:24 +0300

    Movie object setup

--- app/src/main/java/com/example/android/popularmoviesapp/Movie.java
+++ app/src/main/java/com/example/android/popularmoviesapp/Movie.java
@@ -8,8 +8,91 @@ package com.example.android.popularmoviesapp;
 // the app so as to easily use it later
 public class Movie {
 
+    //the following are the paramters taken from the API for each entry and used in the app
+    private String mPosterPath;
+    private Boolean mAdult;
+    private String mOverview;
+    private String mReleaseDate;
+    private int mId;
+    private String mOriginalTitle;
+    private String mOriginalLanguage;
+    private String mTitle;
+    private String mBackdropPath;
+    private float mPopularity;
+    private int mVoteCount;
+    private Boolean mVideo;
+    private double mVoteAverage;
 
-    public void Movie(){
+    public Movie(String posterPath, Boolean adult, String overview, String releaseDate, int id,
+                 String originalTitle, String originalLanguage, String title, String backdropPath,
+                 float popularity, int voteCount, Boolean video, double voteAverage) {
 
+        mPosterPath = posterPath;
+        mAdult = adult;
+        mOverview = overview;
+        mReleaseDate = releaseDate;
+        mId = id;
+        mOriginalTitle = originalTitle;
+        mOriginalLanguage = originalLanguage;
+        mTitle = title;
+        mBackdropPath = backdropPath;
+        mPopularity = popularity;
+        mVoteCount = voteCount;
+        mVideo = video;
+        mVoteAverage = voteAverage;
     }
+
+    //the following are the public get methods to call parameters from the movie object as needed
+    public String getPosterPath() {
+        return mPosterPath;
+    }
+
+    public Boolean getAdult() {
+        return mAdult;
+    }
+
+    public String getOverview() {
+        return mOverview;
+    }
+
+    public String getReleaseDate() {
+        return mReleaseDate;
+    }
+
+    public int getID() {
+        return mId;
+    }
+
+    public String getOriginalTitle() {
+        return mOriginalTitle;
+    }
+
+    public String getOriginalLanguage() {
+        return mOriginalLanguage;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getBackdropPath() {
+        return mBackdropPath;
+    }
+
+    public float getPopularity() {
+        return mPopularity;
+    }
+
+    public int getVoteCount() {
+        return mVoteCount;
+    }
+
+    public boolean getVideo() {
+        return mVideo;
+    }
+
+    public double getVoteAverage() {
+        return mVoteAverage;
+    }
+
 }
--- app/src/main/java/com/example/android/popularmoviesapp/MoviesFragment.java
+++ app/src/main/java/com/example/android/popularmoviesapp/MoviesFragment.java
@@ -1,12 +1,6 @@
 package com.example.android.popularmoviesapp;
 
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 
 /**
@@ -20,29 +14,7 @@ public class MoviesFragment extends Fragment {
 
     }
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-        View rootView = inflater.inflate(R.layout.fragment_movies,container,false);
-
-        return rootView;
-    }
 
 
-    public class fetchMovies extends AsyncTask<Void, Void, Void> {
-
-        @Override
-        protected Void doInBackground(Void... params) {
-            return null;
-        }
-
-
-    }
 
 }
