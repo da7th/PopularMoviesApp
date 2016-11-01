36a113a635d3b1159fa5e87d90b315dbb02df804
da7th-laptop <m.da7th@gmail.com>
2016-11-02 00:33:58 +0200

    visual and ui improvements

--- app/src/main/java/com/example/android/popularmoviesapp/SettingsActivity.java
+++ app/src/main/java/com/example/android/popularmoviesapp/SettingsActivity.java
@@ -1,10 +1,13 @@
 package com.example.android.popularmoviesapp;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 
@@ -89,5 +92,12 @@ public class SettingsActivity extends AppCompatPreferenceActivity
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Nullable
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public Intent getParentActivityIntent() {
+        return super.getParentActivityIntent().addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
     }
 }
